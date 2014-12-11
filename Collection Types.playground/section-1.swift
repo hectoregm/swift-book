4bdc74bf6a0d9227aca4de9e07c5e474b72dd8d2
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-12-11 12:15:46 -0600

    Finished Collection Types section.

--- Collection Types.playground/section-1.swift
+++ Collection Types.playground/section-1.swift
@@ -55,4 +55,60 @@ var sixDoubles = threeDoubles + anotherThreeDoubles
 
 // Dictionaries
 
+var airports: [String: String] = ["YYZ": "Toronto Pearson", "DUB": "Dublin"]
 
+println("The airports dictionary contains \(airports.count) items.")
+
+if airports.isEmpty {
+    println("The airports dictionary is empty.")
+} else {
+    println("The airports dictionary is not empty.")
+}
+
+airports["LHR"] = "London"
+airports
+
+airports["LHR"] = "London Heathrow"
+airports
+
+if let oldValue = airports.updateValue("Dublin Aiport", forKey: "DUB") {
+    println("The old value for DUB was \(oldValue).")
+}
+
+if let airportName = airports["DUB"] {
+    println("The name of the airport is \(airportName)")
+} else {
+    println("The airport is not in the airports dictionary.")
+}
+
+airports["APL"] = "Apple International"
+airports
+airports["APL"] = nil
+airports
+
+if let removedValue = airports.removeValueForKey("DUB") {
+    println("The removed airport's name is \(removedValue).")
+} else {
+    println("The airports dictionary does not contain a value for DUB.")
+}
+
+for (airportCode, airportName) in airports {
+    println("\(airportCode): \(airportName)")
+}
+
+for airportCode in airports.keys {
+    println("Airport code: \(airportCode)")
+}
+
+for airportName in airports.values {
+    println("Airport name: \(airportName)")
+}
+
+let airportCodes = [String](airports.keys)
+
+let airportNames = [String](airports.values)
+
+
+var namesOfIntegers = [Int: String]()
+namesOfIntegers[16] = "sixteen"
+namesOfIntegers = [:]
