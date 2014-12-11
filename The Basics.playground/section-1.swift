c535a83a21642ba8ea83ddece5c364919456efa9
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-12-10 20:55:27 -0600

    Finished The Basics section

--- The Basics.playground/section-1.swift
+++ The Basics.playground/section-1.swift
@@ -70,3 +70,106 @@ let hexadecimalInteger = 0x11     // 17 in hexadecimal notation
 let decimalDouble = 12.1875
 let exponentDouble = 1.21875e1
 let hexadecimalDouble = 0xC.3p0
+
+let paddedDouble = 000123.456
+let oneMillion = 1_000_000
+let justOverOneMillion = 1_000_000.000_000_1
+
+
+// Numeric Type Conversion
+
+// let cannotBeNegative: UInt8 = -1
+// let tooBig: Int8 = Int8.max + 1
+
+let twoThousand: UInt16 = 2_000
+let one: UInt8 = 1
+let twoThousandAndOne = twoThousand + UInt16(one)
+
+let three = 3
+let pointOneFourOneFiveNine = 0.14159
+let pi2 = Double(three) + pointOneFourOneFiveNine
+
+let integerPi = Int(pi2)
+
+// Type Aliases
+typealias AudioSample = UInt16
+var maxAmplitudeFound = AudioSample.min
+
+
+// Booleans
+let orangesAreOrange = true
+let turnipsAreDelicious = false
+
+if turnipsAreDelicious {
+    println("Mmm, tasty turnips!")
+} else {
+    println("Eww, turnips are horrible.")
+}
+
+let i = 1
+if i == 1 {
+    // valid
+}
+
+
+// Tuples
+let http404Error = (404, "Not Found")
+
+let (statusCode, statusMessage) = http404Error
+println("The satus code is \(statusCode)")
+println("The status message is \(statusMessage)")
+
+let (justTheStatusCode, _) = http404Error
+println("The status code is \(justTheStatusCode)")
+
+println("The status code is \(http404Error.0)")
+println("The status message is \(http404Error.1)")
+
+let http202Status = (statusCode: 200, description: "OK")
+println("The status code is \(http202Status.statusCode)")
+println("The status message is \(http202Status.description)")
+
+
+// Optionals
+
+let possibleNumber = "123"
+let convertedNumber = possibleNumber.toInt()
+
+var serverResponseCode: Int? = 404
+serverResponseCode = nil
+
+var surveryAnswer: String?
+surveryAnswer
+
+if convertedNumber != nil {
+    println("convertedNumber contains some integer value.")
+}
+
+if convertedNumber != nil {
+    println("convertedNumber has an integer value of \(convertedNumber!).")
+}
+
+// Optional Binding
+if let actualNumber = possibleNumber.toInt() {
+    println("\'\(possibleNumber)\' has an integer value of \(actualNumber)")
+} else {
+    println("\'\(possibleNumber)\' could not be converted to an integer")
+}
+
+let possibleString: String? = "An optional string."
+let forcedString: String = possibleString! // requires an exclamation mark
+
+let assumedString: String! = "An implicitly unwrapped optional string."
+let implicitString: String = assumedString // no need for an exclamation mark
+
+if assumedString != nil {
+    println(assumedString)
+}
+
+if let definiteString = assumedString {
+    println(definiteString)
+}
+
+// Assertions
+let age = -3
+//assert(age >= 0, "A person's age cannot be less than zero") error!!
