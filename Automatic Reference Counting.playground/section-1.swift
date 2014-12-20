7f2bd2d948b957ed8740de40e5f5aaa17a3e6d84
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-12-20 14:02:13 -0800

    ARC for closures

--- Automatic Reference Counting.playground/section-1.swift
+++ Automatic Reference Counting.playground/section-1.swift
@@ -22,7 +22,6 @@ class Apartment {
     }
 }
 
-
 var reference1: Person?
 var reference2: Person?
 var reference3: Person?
@@ -75,3 +74,47 @@ juan = Customer(name: "John Appleseed")
 juan!.card = CreditCard(number: 1234_5678_9012_3456, customer: juan!)
 john = nil
 
+class Country {
+    let name: String
+    let capitalCity: City!
+    init(name: String, capitalName: String) {
+        self.name = name
+        self.capitalCity = City(name: capitalName, country: self)
+    }
+}
+
+class City {
+    let name: String
+    unowned let country: Country
+    init(name: String, country: Country) {
+        self.name = name
+        self.country = country
+    }
+}
+var country = Country(name: "Canada", capitalName: "Ottawa")
+println("\(country.name)'s capital city is called \(country.capitalCity.name)'")
+
+// Strong Reference Cycles for Closures
+
+class HTMLElement {
+    let name: String
+    let text: String?
+    lazy var asHTML: () -> String = {
+        if let text = self.text {
+            return "<\(self.name)>\(text)</\(self.name)>"
+        } else {
+            return "<\(self.name) />"
+        }
+    }
+    
+    init(name: String, text: String? = nil) {
+        self.name = name
+        self.text = text
+    }
+    
+    deinit {
+        println("\(name) is being deinitialized")
+    }
+}
+var paragraph: HTMLElement? = HTMLElement(name: "p", text: "hello, world")
+println(paragraph!.asHTML())
