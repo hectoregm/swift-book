066992151f37abe453d3c1562ce2b2a8aca19b16
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-12-21 15:04:28 -0800

    Finished first part of Extensions section

---
+++ Extensions.playground/section-1.swift
@@ -0,0 +1,42 @@
+// Playground for Extensions section
+
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1_000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+let oneInch = 25.4.mm
+println("One inch is \(oneInch) meters")
+let threeFeet = 3.ft
+println("Three feet is \(threeFeet) meters")
+
+let aMarathon = 42.km + 195.m
+println("A marathon is \(aMarathon) meters long")
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+
+struct Point {
+    var x = 0.0, y = 0.0
+}
+
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+let defaultRect = Rect()
+let memberwiseRect = Rect(origin: Point(x: 2.0, y: 2.0), size: Size(width: 5.0, height: 5.0))
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+// Methods
