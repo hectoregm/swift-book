0e6985e8f3f9173a6f4030dba5c7d497d3f96b14
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-12-21 19:51:25 -0800

    Finished Extensions section

--- Extensions.playground/section-1.swift
+++ Extensions.playground/section-1.swift
@@ -38,5 +38,75 @@ extension Rect {
         self.init(origin: Point(x: originX, y: originY), size: size)
     }
 }
+let centerRect = Rect(center: Point(x: 4.0, y: 4.0), size: Size(width: 3.0, height: 3.0))
 
 // Methods
+
+extension Int {
+    func repetitions(task: () -> ()) {
+        for i in 0..<self {
+            task()
+        }
+    }
+}
+
+3.repetitions {
+    println("Hello!")
+}
+
+extension Int {
+    mutating func square() {
+        self = self * self
+    }
+}
+var someInt = 3
+someInt.square()
+
+// Subscripts
+
+extension Int {
+    subscript(var digitIndex: Int) -> Int {
+        var decimalBase = 1
+        while digitIndex > 0 {
+            decimalBase *= 10
+            --digitIndex
+        }
+        return (self / decimalBase) % 10
+    }
+}
+74381295[0]
+74381295[1]
+74381295[2]
+
+// Nested Types
+
+extension Int {
+    enum Kind {
+        case Negative, Zero, Positive
+    }
+    
+    var kind: Kind {
+        switch self {
+        case 0:
+            return .Zero
+        case let x where x > 0:
+            return .Positive
+        default:
+            return .Negative
+        }
+    }
+}
+
+func printIntegerKinds(numbers: [Int]) {
+    for number in numbers {
+        switch number.kind {
+        case .Negative:
+            print("- ")
+        case .Zero:
+            print("0 ")
+        case .Positive:
+            print("+ ")
+        }
+    }
+}
+printIntegerKinds([3, 19, -27, 0, -6, 0, 7])
